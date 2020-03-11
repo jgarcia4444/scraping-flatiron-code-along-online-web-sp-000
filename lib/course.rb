c654421acd78ef1bdc08d0358686cdc12702569f
Jake Garcia <bballwiz4@gmail.com>
2020-03-11 06:41:16 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -4,9 +4,9 @@ class Course
 
   attr_accessor :title, :schedule, :description
 
-  # def initialize
-  #   self.all << self
-  # end
+  def initialize
+    self.all << self
+  end
 
   def self.all
     @@all
